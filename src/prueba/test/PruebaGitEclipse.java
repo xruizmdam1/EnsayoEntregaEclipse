5ebf63b03432d4c25420dccb97271e58a4157396
cgoncalvesadam1 <cgoncalvesadam1@gmail.com>
2021-03-25 18:02:35 +0100

    Primer commit de una colaboradora. Saludo a Isabel.

--- src/prueba/test/PruebaGitEclipse.java
+++ src/prueba/test/PruebaGitEclipse.java
@@ -7,7 +7,7 @@ public class PruebaGitEclipse {
 	public static void main(String[] args) {
 		Saludo saludo = new Saludo();
 		System.out.println(saludo.saludar("Juan"));
-		 
+		System.out.println(saludo.saludar("Isabel"));
 
 	}
 
